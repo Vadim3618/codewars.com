b4ddea1c39d61ffdf8ea9d73096942b35832a2fb
Vadim3618 <vdmdzr@gmail.com>
2022-02-24 12:20:10 +0300

    return binary (my solution)

--- codewarsThirtyTwo.js
+++ codewarsThirtyTwo.js
@@ -1,17 +1,21 @@
 // Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
 
 // The binary number returned should be a string.
-
+//my solution
 function addBinary(a, b) {
    let num = a + b
-   let binary = (num % 2).toString()
-   for (; num > 1;) {
-      num = parseInt(num / 2)
-      binary = (num % 2) + (binary)
+   let binary = []
+   while (num > 0) {
+      if (num % 2 == 1) binary.unshift(1)//push 1 to begining array
+      else binary.unshift(0)//push 0 in started array
+      num = Math.floor(num / 2)//remove remainder
    }
-   return binary
+   return binary.join('')//output sting
 }
+
 console.log(addBinary(5, 9))
 
 // solution 2 (smart)
-// return (a + b).toString(2)
+// function addBinary(a, b) {
+//    return (a + b).toString(2)
+// }
