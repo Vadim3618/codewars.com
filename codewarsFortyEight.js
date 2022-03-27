cfe6aad699fe626bbfb01a943ea3a5c83b26f373
Vadim3618 <vdmdzr@gmail.com>
2022-03-27 20:50:48 +0300

    codewarsFortyEight.js

---
+++ codewarsFortyEight.js
@@ -0,0 +1,19 @@
+// Remove First and Last Character
+
+
+function removeChar(str) {
+    let newStr = ''
+    for (let i = 1; i < str.length - 1; i++) {
+        newStr += str[i]
+    }
+ return newStr
+}
+
+// or
+// function removeChar(str) {
+//     return str.slice(1, -1);
+// }
+
+console.log(removeChar('eloquent'))// 'loquen'
+console.log(removeChar('country'))// 'ountr'
+console.log(removeChar('person'))// 'erso'
