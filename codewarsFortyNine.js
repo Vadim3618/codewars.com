e6aa445439be56fcbd80f923283b0dd80b6d7b40
Vadim3618 <vdmdzr@gmail.com>
2022-03-28 17:51:04 +0300

    Find the odd int (return a number that occurs an odd number of times)

---
+++ codewarsFortyNine.js
@@ -0,0 +1,38 @@
+Find the odd int
+// Given an array of integers, find the one that appears an odd number of times.
+//
+//   There will always be only one integer that appears an odd number of times.
+//
+//   Examples
+//   [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+//my decision
+function findOdd(Arr) {
+    let obj = {}
+    for (let i = 0; i < Arr.length; i++) {
+        (Arr[i] in obj) ? obj[Arr[i]] += 1 : obj[Arr[i]] = 1
+    }
+    for (let key in obj) {
+        if (obj[key] % 2 === 1) return +key
+    }
+}
+
+//smart decision
+// function findOdd(A) {
+//     return A.find((number) => A.filter((n) => number === n).length % 2)
+// }
+// or
+// function findOdd(A) {
+//     return A.reduce(function(c,v){return c^v;},0);
+// }
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))//5
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]))//-1
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]))//5
+
+
+
