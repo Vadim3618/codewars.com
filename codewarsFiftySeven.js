b3b04049f95a77cb6624561e3ef273d0a4b767b9
vdmdzr <vdmdzr@gmail.com>
2022-04-21 12:10:27 +0300

    Fibonacci, Tribonacci Sequence(optimized solution)

--- codewarsFiftySeven.js
+++ codewarsFiftySeven.js
@@ -11,7 +11,7 @@ function tribonacci(sign, n) {
         b = c
         c = d
     }
-    return n === 0 ? [] : n === 1 ? sign.slice(0, 1) : n === 2 ? sign.slice(0, 2) : sign
+    return sign.slice(0, n)
 }
 
 console.log(tribonacci([1, 1, 1], 10))//,[1,1,1,3,5,9,17,31,57,105])
