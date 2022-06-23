ec7fa87cf3077a2263bda17d618ade92f040f4e5
vdmdzr <vdmdzr@gmail.com>
2022-06-23 20:20:23 +0300

    Create Phone Number

---
+++ codewarsSixtySeven.js
@@ -0,0 +1,10 @@
+//Create Phone Number
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+function createPhoneNumber(numbers) {
+    return `(${numbers.slice(0, 3)}) ${numbers.slice(3, 6)}-${numbers.slice(6)}`.split(',').join('')
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))//"(123) 456-7890");
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))//"(111) 111-1111");
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))//"(123) 456-7890");
